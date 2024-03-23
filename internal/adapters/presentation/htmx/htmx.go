ef5b1a97f31b92abe99845bbf3bd6f18135980b3
Dwayne Sutherland <dwayne@thisismother.com>
2024-03-23 17:25:35 +0000

    added basic templ setup

--- internal/adapters/presentation/htmx/htmx.go
+++ internal/adapters/presentation/htmx/htmx.go
@@ -3,6 +3,7 @@ package htmx
 import (
 	"net/http"
 
+	"github.com/Delta-a-Sierra/wedding_website/internal/adapters/presentation/htmx/templates"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 )
@@ -11,7 +12,7 @@ func Start() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World!"))
+		templates.Home("test").Render(r.Context(), w)
 	})
 	http.ListenAndServe(":3000", r)
 }
