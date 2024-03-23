9d33af6749316ee74729e5ed4e88b1941e1f7fdb
Dwayne Sutherland <dwayne@thisismother.com>
2024-03-23 14:47:42 +0000

    added logic to main for loading config and starting htmx adapter

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/Delta-a-Sierra/wedding_website/internal/adapters/presentation/htmx"
+	"github.com/Delta-a-Sierra/wedding_website/pkg/config"
+)
+
+func main() {
+	configHandler, err := config.NewHandler()
+	if err != nil {
+		panic(fmt.Errorf("config.NewHandler: %w", err))
+	}
+	conf, err := configHandler.LoadConfig()
+	if err != nil {
+		panic(fmt.Errorf("configHandler.LoadConfig: %w", err))
+	}
+	if conf.PresentationMethod == config.HTMXPM {
+		htmx.Start()
+	}
+}
